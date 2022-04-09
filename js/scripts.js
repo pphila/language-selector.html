c67e4ddcebb6ce261188c72633708783255bde00
pphila <efl2ice89@gmail.com>
2022-04-08 19:11:23 -0700

    add initial scripts

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+function languageGenie(surveyForm) {
+  if (experience === ) {
+    $("#ruby").show();
+  };
+}
+
+$(document).ready(function() {
+  $("#survey-form").submit(function(event) {
+    event.preventDefault();
+    const experience = $("#experience").val();
+    const interest = $("input:radio[name=interest]:checked").val();
+    const trueFalse = $("input:radio[name=torf]:checked").val();
+    const processing = $("input:radio[name=processing]:checked").val();
+    const movies = $("input:radio[name=movies]:checked").val();
+
+    surveyForm(languageGenie)
+  })
+});
+
