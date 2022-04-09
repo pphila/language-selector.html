9a15328af054c360921bda432d23313b6bee3ed1
pphila <efl2ice89@gmail.com>
2022-04-09 10:47:57 -0700

    WIP for  return function script

--- js/scripts.js
+++ js/scripts.js
@@ -2,11 +2,13 @@ $(document).ready(function() {
   $("#survey-form").submit(function(event) {
     event.preventDefault();
     
-    const experience = $("#experience").val();
-    const interest = $("input:radio[name=interest]:checked").val();
-    const trueFalse = $("input:radio[name=torf]:checked").val();
-    const processing = $("input:radio[name=processing]:checked").val();
-    const movies = $("input:radio[name=movies]:checked").val();
+    const experienceInput = parseInt($("#experience").val());
+    const interestInput = $("input:radio[name=interest]:checked").val();
+    const trueFalseInput = $("input:radio[name=torf]:checked").val();
+    const procInput = $("input:radio[name=processing]:checked").val();
+    const moviesInput = $("input:radio[name=movies]:checked").val();
+
+    if (experienceInput + interestInput + trueFalseInput + procInput + moviesInput =)
   })
 });
 
